7cb45f734d8d95663bd8239529f64e2362bf5384
ArbazSk <ArbazSk@arbazshaikh513@gmail.com>
2019-04-18 23:46:50 +0530

    completed

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask,render_template,url_for
-from flask import request,abort,Response,redirect
-from summaries_Rough import summaries
+from flask import request
+from summaries import summaries
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,10 +12,8 @@ def summary():
     if request.method == 'POST':
         raw_text = request.form['inputText']
         final_summary = summaries(raw_text)
-        print(final_summary)
 
-    return render_template('output.html',result=final_summary,input_text=raw_text)
-
+    return render_template('index.html',result=final_summary)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
--- summaries.py
+++ summaries.py
@@ -1,108 +1,65 @@
 import numpy as np
 import pandas as pd
-import nltk
-#nltk.download('punkt')
 import re
 
+def summaries(raw_text):
+  from nltk.tokenize import sent_tokenize
 
-df = pd.read_csv("tennis_articles_v4.csv")
+  # Extract word vectors
+  word_embeddings = {}
+  f = open('glove/glove.6B.100d.txt', encoding='utf-8')
+  for line in f:
+      values = line.split()
+      word = values[0]
+      coefs = np.asarray(values[1:], dtype='float32')
+      word_embeddings[word] = coefs
+  f.close()
 
+  # remove punctuations, numbers and special characters
+  clean_sentences = re.sub(r'\[\d+\]*', "",raw_text)
+  sentences = sent_tokenize(clean_sentences)
+  clean_sentences = pd.Series(sentences).str.replace("[^a-zA-Z]", " ")
+  # make alphabets lowercase
+  clean_sentences = [n.lower() for n in clean_sentences]
 
-from nltk.tokenize import sent_tokenize
-sentences = []
-for s in df['article_text']:
-  sentences.append(sent_tokenize(s))
+  from nltk.corpus import stopwords
+  stop_words = stopwords.words('english')
 
-sentences = [y for x in sentences for y in x] # flatten list
+  # function to remove stopwords
+  def remove_stopwords(sen):
+      sen_new = " ".join([i for i in sen if i not in stop_words])
+      return sen_new
 
-# Extract word vectors
-word_embeddings = {}
-f = open('glove/glove.6B.100d.txt', encoding='utf-8')
-for line in f:
-    values = line.split()
-    word = values[0]
-    coefs = np.asarray(values[1:], dtype='float32')
-    word_embeddings[word] = coefs
-f.close()
+  # remove stopwords from the sentences
+  clean_sentences = [remove_stopwords(r.split()) for r in clean_sentences]
 
+  sentence_vectors = []
+  for i in clean_sentences:
+    if len(i) != 0:
+      v = sum([word_embeddings.get(w, np.zeros((100,))) for w in i.split()])/(len(i.split())+0.001)
+    else:
+      v = np.zeros((100,))
+    sentence_vectors.append(v)
 
+  # similarity matrix
+  sim_mat = np.zeros([len(sentences), len(sentences)])
 
-# remove punctuations, numbers and special characters
-clean_sentences = pd.Series(sentences).str.replace("[^a-zA-Z]", " ")
+  from sklearn.metrics.pairwise import cosine_similarity
 
-# make alphabets lowercase
-clean_sentences = [s.lower() for s in clean_sentences]
+  for i in range(len(sentences)):
+    for j in range(len(sentences)):
+      if i != j:
+        sim_mat[i][j] = cosine_similarity(sentence_vectors[i].reshape(1,100), sentence_vectors[j].reshape(1,100))[0,0]
 
+  import networkx as nx
 
+  nx_graph = nx.from_numpy_array(sim_mat)
+  scores = nx.pagerank(nx_graph)
 
-# nltk.download('stopwords')
+  ranked_sentences = sorted((s for i,s in enumerate(sentences)), reverse=True)
 
+  # print the length of the ranked sentence
+  length = len(ranked_sentences) // 3
+  output = " ".join(ranked_sentences[:length])
 
-from nltk.corpus import stopwords
-stop_words = stopwords.words('english')
-
-
-
-# function to remove stopwords
-def remove_stopwords(sen):
-    sen_new = " ".join([i for i in sen if i not in stop_words])
-    return sen_new
-
-
-
-# remove stopwords from the sentences
-clean_sentences = [remove_stopwords(r.split()) for r in clean_sentences]
-
-
-
-# Extract word vectors
-# word_embeddings = {}
-# f = open('glove/glove.6B.100d.txt', encoding='utf-8')
-# for line in f:
-#     values = line.split()
-#     word = values[0]
-#     coefs = np.asarray(values[1:], dtype='float32')
-#     word_embeddings[word] = coefs
-# f.close()
-
-
-sentence_vectors = []
-for i in clean_sentences:
-  if len(i) != 0:
-    v = sum([word_embeddings.get(w, np.zeros((100,))) for w in i.split()])/(len(i.split())+0.001)
-  else:
-    v = np.zeros((100,))
-  sentence_vectors.append(v)
-
-
-# similarity matrix
-sim_mat = np.zeros([len(sentences), len(sentences)])
-
-
-from sklearn.metrics.pairwise import cosine_similarity
-#import sklearn
-
-
-for i in range(len(sentences)):
-  for j in range(len(sentences)):
-    if i != j:
-      sim_mat[i][j] = cosine_similarity(sentence_vectors[i].reshape(1,100), sentence_vectors[j].reshape(1,100))[0,0]
-
-
-# print("Printing The Sim_mat:")
-# print(sim_mat)
-
-import networkx as nx
-
-nx_graph = nx.from_numpy_array(sim_mat)
-scores = nx.pagerank(nx_graph)
-
-
-ranked_sentences = sorted(((scores[i],s) for i,s in enumerate(sentences)), reverse=True)
-
-
-
-# Extract top 10 sentences as the summary
-for i in range(10):
-  print(f"{i+1}-->" + ranked_sentences[i][1])
-  
+  return output
